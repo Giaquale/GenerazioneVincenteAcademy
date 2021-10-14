8d68b0e6796d77140097eefc965adffe7353f81b
Krahll <andrea.innacolo@gmail.com>
2021-10-14 17:06:30 +0200

    Update Rugbista.java

--- src/gva/model/Rugbista.java
+++ src/gva/model/Rugbista.java
@@ -4,7 +4,11 @@ public class Rugbista extends Sportivo {
 
 	public Rugbista(String nome, String cognome, short eta, Sesso sesso, Ruolo ruolo) {
 		super(nome, cognome, eta, sesso, ruolo);
+<<<<<<< HEAD
 	
+=======
+		
+>>>>>>> e58d6604445ddfdfe7d8165d0ec735ce3a34e644
 	}
 
 }
