b90d81b988818e3dfddeae3671ade63de0a2989b
AleAval <92525741+AleAval@users.noreply.github.com>
2021-10-14 17:49:19 +0200

    Update Baskettista.java

--- src/gva/model/Baskettista.java
+++ src/gva/model/Baskettista.java
@@ -1,13 +1,24 @@
 package gva.model;
 
 public abstract class Baskettista extends Sportivo{
+	
+	private short posizione;
 
 	public Baskettista(String nome, String cognome, short eta, Sesso sesso, Ruolo ruolo) {
 		super(nome, cognome, eta, sesso, ruolo);
 	}
-	
-	public String toString() {
-		return "Giocatore di Basket{}";
+
+	public short getPosizione() {
+		return posizione;
 	}
+
+	public void setPosizione(short posizione) {
+		if (posizione <= 0 || posizione > 5) {
+			System.out.println("IMPOSSIBILE");
+		}
+		this.posizione = posizione;
+	}
+	
+	
 	
 }
